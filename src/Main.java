1406426ea3da480f39cf1bfe056d6217c2a8a657
ril4u <ril4you@gmail.com>
2023-02-24 19:57:10 +0500

    Add average

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Github!");
-        int[] managerSales = {10,5,17,0,11,7};
+        int[] managerSales = {10, 5, 17, 3, 11, 7};
         SalesManager manager1 = new SalesManager(managerSales);
-        System.out.println("Продажи за полгода: " + Arrays.toString(managerSales));
-        System.out.println("Продажи в лучшем месяце: " + manager1.max());
+        System.out.println("Продажи: " + Arrays.toString(managerSales));
+        //System.out.println("Продажи в лучшем месяце: " + manager1.max());
+        //System.out.println("Max: " + manager1.max());
+        //System.out.println("Min: " + manager1.min());
+        System.out.println("Среднее обрезанное значение: " + manager1.average());
+
     }
 }
