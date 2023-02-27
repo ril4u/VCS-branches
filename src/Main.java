ad751789986c0e426872f30eba7a858ab1630108
ril4u <ril4you@gmail.com>
2023-02-27 22:52:47 +0500

    uncomment old methods + reformat to long

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
-        Long[] managerSales = {10L,5L,17L,0L,11L,7L};
+        long[] managerSales = {10L,5L,17L,0L,11L,7L};
         SalesManager manager1 = new SalesManager(managerSales);
         System.out.println("Продажи: " + Arrays.toString(managerSales));
         //System.out.println("Продажи в лучшем месяце: " + manager1.max());
-        //System.out.println("Max: " + manager1.max());
-        //System.out.println("Min: " + manager1.min());
+        System.out.println("Max: " + manager1.max());
+        System.out.println("Min: " + manager1.min());
         System.out.println("Среднее обрезанное значение: " + manager1.average());
 
     }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,44 +1,47 @@
 public class SalesManager {
-    protected Long[] sales;
+    protected long[] sales;
 
-    public SalesManager(Long[] sales) {
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
-    /* Методы использовались в предыдущих заданиях
-        public int max() {
-            int max = -1;
-            for (int sale : sales) {
-                if (sale > max) {
-                    max = sale;
-                }
+
+    //Методы использовались в предыдущих заданиях
+
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
+            if (sale > max) {
+                max = sale;
             }
-            return max;
         }
-        public int min() {
-            int min = 1000;
-            for (int sale : sales) {
-                if (sale < min) {
-                    min = sale;
-                }
+        return max;
+    }
+
+    public long min() {
+        long min = 1000;
+        for (long sale : sales) {
+            if (sale < min) {
+                min = sale;
             }
-            return min;
         }
-    */
-    public double average() {
-        double average = 0;
-        for (Long sale : sales) {
+        return min;
+    }
+
+    public long average() {
+        long average = 0;
+        for (long sale : sales) {
             average += sale;
         }
         //определение максимального значения
-        Long max = -1L;
-        for (Long sale : sales) {
+        long max = -1L;
+        for (long sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         //определение минимального
-        Long min = 1000L;
-        for (Long sale : sales) {
+        long min = 1000L;
+        for (long sale : sales) {
             if (sale < min) {
                 min = sale;
             }
